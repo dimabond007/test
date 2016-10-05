217e569011ce2016de04ea3c2e7f9843c3d08a99
unknown <07489@A311_04.zp.itstep.org>
2016-10-05 19:45:34 +0300

    first commit

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
+	<script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
+</head>
+<body>
+	@section('menu')
+	<div class='container'>
+		<ul class='nav nav-pills'>
+			<li><a href="{{url('home')}}">Home</a></li>			
+			<li><a href="{{url('addcountry')}}">Add New</a></li>
+		</ul>
+	</div>	
+	<div class="container col-md-12 col-lg-12 col-sm-12">
+	@yield('content')
+	</div>
+</body>
+</html>
